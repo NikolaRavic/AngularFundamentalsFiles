dc8fede58a90d302c1178d30969573aea1cbd12a
Nikola Ravic <nikola.ravic@gmail.com>
2016-11-10 17:35:04 +0100

    10.11.2016. 17:33

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,64 @@
+/**
+ * Created by nravic on 10.11.2016.
+ */
+'use strict'
+
+eventsApp.controller('EventController',
+    function ($scope) {
+        $scope.event = {
+            name: 'Angular boot camp',
+            date: 1359781015626,
+            time: '10:30 am',
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            },
+            imageUrl: '/img/angularjs-logo.png',
+            sessions:[
+                {
+                    name: 'Directives Masterclass',
+                    creatorName: 'Bob Smith',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn the ins and outs of directives.',
+                    upVoteCount: 0
+
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName: 'Nikola Ravic',
+                    duration: '1 hr',
+                    level: 'Beginner',
+                    abstract: 'In this session you will learn the ins and outs of directives.',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Snovak',
+                    duration: '1 hr',
+                    level: 'Intermediate',
+                    abstract: 'In this session you will learn the ins and outs of directives.',
+                    upVoteCount: 0
+                }
+            ]
+        };
+        $scope.upVoteSession= function (session) {
+            session.upVoteCount++;
+            $scope.notZero=true;
+        }
+        $scope.downVoteSession= function (session) {
+            if(session.upVoteCount>0){
+                $scope.notZero=true;
+                session.upVoteCount--;
+            }
+            if(session.upVoteCount<1){
+                $scope.notZero=false;
+            }
+        }
+        $scope.snippet = '<span style="color:red">Hi there!</span>';
+        $scope.boolValue = true;
+        $scope.mystyle = {color: 'red'};
+        $scope.myclass = "blue";
+        $scope.sortOrder = 'name';
+    });
