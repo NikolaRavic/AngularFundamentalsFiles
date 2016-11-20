a74d3159e5ad700176ad5b0622bd6d9314ca3374
Nikola <nikola.ravic@gmail.com>
2016-11-21 00:34:16 +0100

    added EventController.js

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,45 +4,8 @@
 'use strict'
 
 eventsApp.controller('EventController',
-    function ($scope) {
-        $scope.event = {
-            name: 'Angular boot camp',
-            date: 1359781015626,
-            time: '10:30 am',
-            location: {
-                address: 'Google Headquarters',
-                city: 'Mountain View',
-                province: 'CA'
-            },
-            imageUrl: '/img/angularjs-logo.png',
-            sessions:[
-                {
-                    name: 'Directives Masterclass',
-                    creatorName: 'Bob Smith',
-                    duration: 1,
-                    level: 'Advanced',
-                    abstract: 'In this session you will learn the ins and outs of directives.',
-                    upVoteCount: 0
-
-                },
-                {
-                    name: 'Scopes for fun and profit',
-                    creatorName: 'Nikola Ravic',
-                    duration: 2,
-                    level: 'Beginner',
-                    abstract: 'In this session you will learn the ins and outs of directives.',
-                    upVoteCount: 0
-                },
-                {
-                    name: 'Well Behaved Controllers',
-                    creatorName: 'Snovak',
-                    duration: 3,
-                    level: 'Intermediate',
-                    abstract: 'In this session you will learn the ins and outs of directives.',
-                    upVoteCount: 0
-                }
-            ]
-        };
+    function ($scope, eventData) {
+        $scope.event = eventData.event;
         $scope.upVoteSession= function (session) {
             session.upVoteCount++;
             $scope.notZero=true;
